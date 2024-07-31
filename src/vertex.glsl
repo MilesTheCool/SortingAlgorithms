c085f50b5b1184f6c4f926ed631d190535b80fbc
= <=>
2024-07-31 15:53:59 -0700

    added perspective matrix for a ortho projection

--- src/vertex.glsl
+++ src/vertex.glsl
@@ -2,7 +2,8 @@
 layout (location = 0) in vec3 pos;
 
 uniform mat4 trans;
+uniform mat4 perspective;
 
 void main() {
-    gl_Position = trans * vec4(pos, 1.0);
+    gl_Position = perspective * trans * vec4(pos, 1.0);
 }
