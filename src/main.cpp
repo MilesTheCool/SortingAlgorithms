fe56393f5619ea59a64916971fc63a9a77d4e5ef
= <=>
2024-07-31 17:08:10 -0700

    get bubble sort to work and draw the change at every step

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
 #include <glad/glad.h>                      // load in OpenGL functions
 #include <GLFW/glfw3.h>                     // OpenGL functions
 #include <iostream>                         // push stuff to terminal
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <cmath>
-#include "shader.hpp"
+#include <glm/glm.hpp>                      // mat4s and vecs
+#include <glm/gtc/matrix_transform.hpp>     // modify trans matrix
+#include <vector>                           // use vector to store nums
+#include <random>                           // to generate random nums
+#include "shader.hpp"                       // custom shader to draw bars
 
+
+/* OPENGL FUNCTIONS FOR SET-UP AND DRAWING */
 
 /// @brief create the window and initialize OpenGL, returning pointer to window
 /// @param width The width in pixels of the screen to be made
@@ -30,11 +33,40 @@ void processInput(GLFWwindow *window);
 /// @param VAO reference to vertex array ID, changes the value of the var passed into it
 void setup_buffers(unsigned int &VBO, unsigned int &EBO, unsigned int &VAO);
 
+/// @brief draw each of the numbers in the array, moving to the right each time, scaling height with value
+/// @param vec a vector holding all the numers involved
+/// @param shader shader object to draw with
+template <class RandomIt>
+void draw_array(RandomIt first, RandomIt last, const Shader* shader, GLFWwindow* window);
 
+
+/* RUN TIME SET-UP */
+
+/// @brief Generate a vector of random numbers ranging from 1-max_num
+/// @param size the number of nums to generate, ie size of vector being made
+/// @param seed the seed for generating 'random' numbers
+/// @param max_num the biggest number possible to generate
+/// @return vector containing random mixed numbers
+std::vector<int> random_array(size_t size, unsigned int seed, unsigned int max_num); 
+
+
+/* SORTING ALGORITHMS - EACH SHOULD ONLY TAKE FIRST AND LAST ITERATORS TO SORT  (plus opengl shader to draw) */
+/// @brief sort a vector or other data type that can be traversed with iterators
+/// from least to biggest, assuming they are full of numbers
+/// at each step, if graphics are enabled, then when a number is overwritten it will be displayed
+/// std::is_sorted() will return true after each one
+
+template <class RandomIt>
+void bubble_sort(RandomIt first, RandomIt last, const Shader* shader, GLFWwindow* window);
+
+
+bool sort = false;
+const int MAX_NUM = 1000;
+const int SIZE = 1000;
 
 int main() {
     // setup opengl
-    GLFWwindow* window = setupWindow(800,600,"Sorting Algorithms");
+    GLFWwindow* window = setupWindow(1000,1000,"Sorting Algorithms");
 
     if (window == nullptr) { 
         std::cout << "ERROR. OPENGL FAILURE" << std::endl;
@@ -42,6 +74,15 @@ int main() {
     }
 
     
+    
+    std::random_device rd;
+    const unsigned int SEED = rd();
+
+    std::vector<int> vec = random_array(SIZE, SEED, MAX_NUM);
+
+    // set up all vars needed for drawing
+
+    const glm::mat4 perspective = glm::ortho(-1.0f, static_cast<float>(SIZE + 1), -1.0f, static_cast<float>(MAX_NUM + 1));
 
     unsigned int VBO, EBO, VAO;
     setup_buffers(VBO, EBO, VAO);
@@ -51,11 +92,9 @@ int main() {
 
     // set uniforms
     shader->use();
-    shader->setMat4("trans", glm::mat4(1.0f));
-    shader->setFloat("height", 1.0f);
+    shader->setMat4("perspective", perspective);
 
-    // height var
-    double height;
+    
 
     // render loop
     while (!glfwWindowShouldClose(window))
@@ -63,18 +102,12 @@ int main() {
         // input
         processInput(window);
 
-        // render stuff
+        draw_array(vec.begin(), vec.end(), shader, window);       
 
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        height = (sin(glfwGetTime()) / 2.0) + 0.5;
-        shader->setFloat("height", static_cast<float>(height));
-
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
-
-        glfwSwapBuffers(window);
-        glfwPollEvents();
+        if (sort) {
+            bubble_sort(vec.begin(), vec.end(), shader, window);
+            sort = false;
+        } 
     }
 
     // delete OpenGL buffers 
@@ -89,7 +122,7 @@ int main() {
     return 0;
 }
 
-// setup window and startup opengl, return refrence to window object
+/* OPEN GL FUNCTIONS FOR DRAWING */
 GLFWwindow* setupWindow(unsigned int width, unsigned int height, const char* title)
 {
         // glfw: initialize and configure
@@ -122,7 +155,6 @@ GLFWwindow* setupWindow(unsigned int width, unsigned int height, const char* tit
     return window;
 }
 
-
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     // make sure the viewport matches the new window dimensions; note that width and 
@@ -136,6 +168,10 @@ void processInput(GLFWwindow *window)
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) 
     {
         glfwSetWindowShouldClose(window, true);
+    }
+    if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS)
+    {
+        sort = true;
     }
 }
 
@@ -175,6 +211,78 @@ void setup_buffers(unsigned int &VBO, unsigned int &EBO, unsigned int &VAO) {
     // unbind the vertex array (unbinds buffers automatically)
     // glBindVertexArray(0);
 
+}
+
+template <class RandomIt>
+void draw_array(RandomIt first, RandomIt last, const Shader* shader, GLFWwindow* window){
+    // render stuff
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+    
+    int index = 0;
+    for (auto i = first; i < last; ++i){
+        // height is num/max 
+        float height = static_cast<float>(*i) / static_cast<float>(MAX_NUM);   // change to floats to avoid integer division
+        shader->setFloat("height", height);
+
+        // transformation, move x to i, scale y to nums[i]
+        glm::mat4 trans{1.0f};
+        trans = glm::translate(trans, glm::vec3(static_cast<float>(index), 0.0f, 0.0f));
+        trans = glm::scale(trans, glm::vec3(1.0f, static_cast<float>(*i), 1.0f));
+        shader->setMat4("trans", trans);
+
+        // draw
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+
+        ++index;  // used to move next rect to the right
+    }
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+}
+
+/* SET UP RUN-TIME DETAILS */
+std::vector<int> random_array(size_t size, unsigned int seed, unsigned int max_num) {
+
+    // create a random number generator
+    std::mt19937 gen(seed);
+    std::uniform_int_distribution<> distrib(1, max_num);
+
+    // preallocate vector with max size needed
+    std::vector<int> vec(size);
+
+    // fill vector with nums
+    for (int& value : vec) {
+        value = distrib(gen);
+    }
+
+    return vec;
+}
+
+/* SORTING ALGORITHMS - EACH SHOULD ONLY TAKE FIRST AND LAST ITERATORS TO SORT */
+
+template <class RandomIt>
+void bubble_sort(RandomIt first, RandomIt last, const Shader* shader, GLFWwindow* window) {
+
+    bool swapped = true;  // end early if no swaps made
+
+    // iterale n-1 times through array
+    for (auto i = first; i < last && swapped; ++i){
+        swapped = false;
+
+        // iterate through each element in array (except the last)
+        for (auto current = first; current < last - 1 - (i - first); ++current){
+
+            // compare adjacent cells, swap if out of order
+            if (*current > *(current + 1)) {
+                std::swap(*current, *(current + 1));
+                swapped = true;
+            }
+        }
+        
+        // draw after at least 1 number is in final resting place
+        draw_array(first, last, shader, window);
+    }
 }
 
 /* EOF */
